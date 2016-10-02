be905d448760e607cf9e2d567156a131df764308
박지수 <wltn000129@gmail.com>
2016-10-02 23:01:04 +0900

    /routes/oauth.js Facebook Social Login Setting Done

--- routes/oauth.js
+++ routes/oauth.js
@@ -5,6 +5,52 @@
 module.exports = init;
 
 function init(app, User, randomString) {
+    var passport = require('passport');
+    var FacebookTokenStrategy = require('passport-facebook-token');
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    passport.serializeUser(function(user, done){
+        done(null, user);
+    });
+
+    passport.deserializeUser(function (obj, done) {
+        done(null, obj);
+    });
+
+    passport.use(new FacebookTokenStrategy({
+        clientId : "",
+        clientSecret : ""
+    }, function (accessToken, refreshToken, profile, done) {
+        console.log(profile);
+        User.findOne({
+            _id : profile.id
+        }, function (err, user) {
+            if(err){
+                return done(err);
+            }
+            if(!user){
+                user = new User({
+                    _id : profile.id,
+                    name : profile.displayName,
+                    email : profile.email,
+                    phone : profile.phone,
+                    reservation : "",
+                    reservation_wating : []
+                });
+                user.save(function (err) {
+                    if(err) console.log(err);
+                    else{
+                        done(null, profile);
+                    }
+                })
+            }
+            else if(user){
+                done(null, profile);
+            }
+        })
+    }));
 
     //function end
 }
