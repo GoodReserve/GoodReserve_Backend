78266ab03be962eefb97a2e6c5615e9f243f5c61
Astoration <ybdh00@naver.com>
2016-10-12 03:37:29 +0900

    create setting page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index',{restaurantName:"레스토랑 태윤 드 프리미엄"});
 });
 
+router.get('setting',function(req,res){
+  res.render('setting',{restaurantName:"레스토랑 태윤 드 프리미엄"});
+});
+
 module.exports = router;
