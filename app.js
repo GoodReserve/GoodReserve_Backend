54fce999ed05377f0244b64fe1b004bb212b2a99
박지수 <wltn000129@gmail.com>
2016-10-16 13:47:52 +0900

    index.js router bug fixed

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', routes);
+// app.use('/', routes);
 app.use('/users', users);
 
 require('./routes/auth.js')(app, User, randomString);
--- routes/index.js
+++ routes/index.js
@@ -6,99 +6,109 @@ function init(app, Reservation, Menu, Restaurant) {
     var router = express.Router();
 
     /* GET home page. */
-    router.get('/', function (req, res, next) {
-        Restaurant.findOne({_id : req.session.restaurant_id}).exec(function (err, result) {
-            if(err){
-                console.log('index.js router.get / db Error');
-                throw err;
-            }
-            res.render('index', {
-                restaurantName: result.name,
-                reserveList: Reservation.find({restaurant_id: req.session.restaurant_id}, function (err, resv_result) {
-                    if (err) {
-                        console.log("routing reservation find Error");
-                        throw err;
-                    }
-                    return resv_result;
-                }),
+    app.get('/', function (req, res, next) {
+        if(req.session == null){
+            res.redirect('/login')
+        }
+        else if(req.session != null){
+            Restaurant.findOne({_id : req.session.restaurant_id}).exec(function (err, result) {
+                if(err){
+                    console.log('index.js router.get / db Error');
+                    throw err;
+                }
+                res.render('index', {
+                    restaurantName: result.name,
+                    reserveList: Reservation.find({restaurant_id: req.session.restaurant_id}, function (err, resv_result) {
+                        if (err) {
+                            console.log("routing reservation find Error");
+                            throw err;
+                        }
+                        return resv_result;
+                    }),
 
-                /*
-                 TODO :
-                 reserveList : Object[]{
-                 startTime
-                 endTime
-                 code
-                 name
-                 callNumber
-                 menu : Object[]{
-                 name
-                 amount
-                 }
-                 totalPrice
-                 }
-                 */
+                    /*
+                     TODO :
+                     reserveList : Object[]{
+                     startTime
+                     endTime
+                     code
+                     name
+                     callNumber
+                     menu : Object[]{
+                     name
+                     amount
+                     }
+                     totalPrice
+                     }
+                     */
+                });
+            })
+        }
+    });
+
+    app.get('/setting', function (req, res) {
+        if(req.session != null){
+            Restaurant.findOne({_id: req.session.restaurant_id}, function (err, result) {
+                if (err) {
+                    console.log('router restaurant find db error');
+                    throw err;
+                }
+                res.render('setting', {
+                    restaurantName: result.name,
+                    menus: Menu.find({restaurant: req.session.restaurant_id}, function (err, menu_result) {
+                        if (err) {
+                            console.log('router setting db error');
+                            throw err;
+                        }
+                        return menu_result;
+                    }),
+                    reserveMin : 0,
+                    reserveMax : result.reservation_max,
+                    seatTypeOne : 0,
+                    seatTypeTwo : 0,
+                    seatTypeFour : 0,
+                    mainDish : "",
+                    target : "",
+                    dates : [],
+                    startTime : 0,
+                    endTime : 0,
+                    reservation_cancel : result.reservation_cancel,
+                    comment : ""
+                    /*
+                     TODO :
+                     menus : Object[]{
+                     name
+                     price
+                     imageUrl
+                     }
+                     reserveMin //null
+                     reserveMax
+                     seatTypeOne //null
+                     seatTypeTwo //null
+                     seatTypeFour //null
+                     mainDish //null
+                     target //null
+                     dates //예약 가능 날짜 배열 //null
+                     startTime //null
+                     endTime //null
+                     reservation_cancel
+                     purchaseType : 0 or 1
+                     comment //null
+                     */
+                });
             });
-        })
-    });
-
-    router.get('/setting', function (req, res) {
-        Restaurant.findOne({_id: req.session.restaurant_id}, function (err, result) {
-            if (err) {
-                console.log('router restaurant find db error');
-                throw err;
-            }
-            res.render('setting', {
-                restaurantName: result.name,
-                menus: Menu.find({restaurant: req.session.restaurant_id}, function (err, menu_result) {
-                    if (err) {
-                        console.log('router setting db error');
-                        throw err;
-                    }
-                    return menu_result;
-                }),
-                reserveMin : 0,
-                reserveMax : result.reservation_max,
-                seatTypeOne : 0,
-                seatTypeTwo : 0,
-                seatTypeFour : 0,
-                mainDish : "",
-                target : "",
-                dates : [],
-                startTime : 0,
-                endTime : 0,
-                reservation_cancel : result.reservation_cancel,
-                comment : ""
-            /*
-             TODO :
-             menus : Object[]{
-             name
-             price
-             imageUrl
-             }
-             reserveMin //null
-             reserveMax
-             seatTypeOne //null
-             seatTypeTwo //null
-             seatTypeFour //null
-             mainDish //null
-             target //null
-             dates //예약 가능 날짜 배열 //null
-             startTime //null
-             endTime //null
-             reservation_cancel
-             purchaseType : 0 or 1
-             comment //null
-             */
-        });
-        });
+        }
+        else if(req.session == null){
+            res.redirect('/login');
+        }
 
     });
 
-    router.get('/login', function (req, res) {
+    app.get('/login', function (req, res) {
         res.render('login', {});
     });
 
-    router.get('/signup', function (req, res) {
+    app.get('/signup', function (req, res) {
         res.render('signup', {});
     });
 }
